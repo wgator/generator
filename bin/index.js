3a3128e9ee71aaa99e39acecc1494e22b1e8257d
wgator <rafael.ja.tinha@gmail.com>
2014-02-04 19:37:50 -0200

    CHANGE: template paths are now set on bin/index.js and resolving in [MODULE_PATH]/.generator - CHANGE: module "optimist" replaced by "commander"

--- bin/index.js
+++ bin/index.js
@@ -10,22 +10,24 @@
 /**
  * Dependencies
  */
-var colors      = require('colors'),
-    optimist    = require('optimist')
-        .usage('Usage: $0 -t npm -n myModule')
-        .alias('h', 'help')
-        .alias('t', 'template')
-        .alias('n', 'name')
-        .alias('s', 'setup')
-        .describe('t', 'Template name (from ~/.generator)')
-        .describe('n', 'Name of project to be generated')
-        .describe('s', 'Initializes the included modules at ~/.generator');
+var colors   = require('colors'),
+    program  = require('commander')
+    pkg      = require('../package.json'),
+    version  = pkg.version;
 
-var argv        = optimist.argv,
-    copy        = require('../lib/copy'),
+    program
+        .version(version)
+        .usage('Usage: $0 -t npm -n myModule')
+        .option('-t --template [template]', 'Template name (from [MODULE_PATH]/.generator)')
+        .option('-n --name [name]', 'Name of project to be generated')
+        .option('-s --setup', 'Initializes the included modules at [MODULE_PATH]/.generator')
+        .parse(process.argv);
+
+var map = {source: __dirname + '/../templates', target: __dirname + '/../.generator'},
+    copy        = require('../lib/copy')(map),
     make        = require('../lib/make'),
     render      = require('../lib/render'),
-    setup       = require('../lib/setup'),
+    setup       = require('../lib/setup')(map),
     walk        = require('../lib/walk');
 
 /**
@@ -36,27 +38,29 @@ function stderr (err) {
     process.exit(1);
 }
 
+//console.log(program.template, program.name);
+//process.exit(0);
+
 /**
  * Execute
  */
-if (argv.help) return optimist.showHelp();
-if (argv.setup) {
+if (program.setup) {
     setup(function (err) {
         if (err) return stderr(err);
         console.log('Done.'.green);
     });
 } else {
     // Check requirements
-    if (typeof argv.template === 'undefined' || argv.name === 'undefined') {
+    if (typeof program.template === 'undefined' || program.name === 'undefined') {
         return stderr('Template (-t) and project name (-n) must be specified. See --help');
     }
 
     // Replicate the template
-    copy(argv.template, argv.name, function (err, path) {
+    copy(program.template, program.name, function (err, path) {
         if (err) return stderr(err);
 
         // Walk and gather variables
-        walk(path, argv.name, function (err, result) {
+        walk(path, program.name, function (err, result) {
             if (err) return stderr(err);
 
             // Render the results of the walk
